b39314b499ce5a5011e5b360f1c68d5506aec52e
Gahamanyi-dev <61550804+Gahamanyi-dev@users.noreply.github.com>
2022-09-15 14:18:35 +0200

    a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *  mul - multiplies two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: multiplication of the 2 inputs
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
